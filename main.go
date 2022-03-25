f46fff97cd78f8e1f0f410d8d1ce72652d5d8083
William Rehwinkel <willrehwinkel@gmail.com>
2022-03-25 19:18:31 -0400

    .gitignore and main.go, environment variables example

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
 	log.Println("peertube-multipart-upload")
+
+	/*
+		reading environment variables example:
+	*/
+	username, uok := os.LookupEnv("PTUNAME")
+	log.Println("environment variable:")
+	log.Println("PTUNAME, ok")
+	log.Println(username, uok)
+
 }
