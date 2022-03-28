fad42793dd240316d206cb2e5e8c23ec3409304e
William Rehwinkel <willrehwinkel@gmail.com>
2022-03-28 00:09:28 -0400

    Revert "remove trailing / from configDir (probably useless)"
    This reverts commit 0f08bf274792f729cb09684846cc443776b0f992.

--- configDir.go
+++ configDir.go
@@ -3,23 +3,22 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 )
 
 var (
 	AppData, _ = GetConfigDir()
 )
 
-func GetConfigDir() (string, error) {
+func GetConfigDir() (appData string, err error) {
 	/*
 		Wrapper around os.UserConfigDir which
 		exits the entire program if the
 		function returns an error for
 		any reason.
 	*/
-	appData, err := os.UserConfigDir()
+	appData, err = os.UserConfigDir()
 	if err != nil {
 		log.Fatalf("Attempt to find user config directory returned the following error: \"%s\"\n", err.Error())
 	}
-	return strings.TrimRight(appData, "/"), err
+	return
 }
